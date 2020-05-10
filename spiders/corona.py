732c35e949f0be7e8fbba0a4401e336826dacdd5
shawon1fb <shawon0187@gmail.com>
2020-05-10 22:20:08 +0600

    initial random

---
+++ spiders/corona.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CoronaSpider(scrapy.Spider):
+    name = 'corona'
+    allowed_domains = ['https://www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/coronavirus//']
+
+    def parse(self, response):
+        temp = response.xpath("//table[@id='main_table_countries_today']/tbody[1]/tr")
+        print("--------------------------------------------------------------------------")
+        print(f"temp length ----------------->>>{len(temp)}")
+        print("--------------------------------------------------------------------------")
+        for t in temp:
+            item = dict()
+            item["Country"] = t.xpath(".//a//text()").get()
+            item["Total_Case"] = t.xpath(".//td[2]/text()").get()
+
+            yield item
+#
