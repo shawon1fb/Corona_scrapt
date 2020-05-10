0e94e7d9c9c7663b46b31c78ebcd9779d065a6db
shawon1fb <shawon0187@gmail.com>
2020-05-10 23:00:03 +0600

    rerun

--- Corona_scrapt/scrapy_runner.py
+++ Corona_scrapt/scrapy_runner.py
@@ -1,6 +1,11 @@
 from scrapy import cmdline
 
-spider_list = ["corona"]
-scraper_name = spider_list[0]
 
-cmdline.execute(f"scrapy crawl {scraper_name}".split())
+def start_project():
+    spider_list = ["corona"]
+    scraper_name = spider_list[0]
+    cmdline.execute(f"scrapy crawl {scraper_name}".split())
+
+
+start_project()
+
